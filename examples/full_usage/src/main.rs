527bb059dccd5d46aab4c554011c7f298a4d6d35
Niko <niko@niko.lgbt>
2025-08-02 19:47:58 -0700

    Fix up `full_usage` example

--- examples/full_usage/src/main.rs
+++ examples/full_usage/src/main.rs
@@ -1,10 +1,10 @@
 #![allow(clippy::uninlined_format_args)]
 
 use hound::{SampleFormat, WavReader};
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use whisper_rs::{FullParams, SamplingStrategy, WhisperContext, WhisperContextParameters};
 
-fn parse_wav_file(path: &Path) -> Vec<i16> {
+fn parse_wav_file(path: PathBuf) -> Vec<i16> {
     let reader = WavReader::open(path).expect("failed to read file");
 
     if reader.spec().channels != 1 {
@@ -27,19 +27,21 @@ fn parse_wav_file(path: &Path) -> Vec<i16> {
 }
 
 fn main() {
-    let arg1 = std::env::args()
-        .nth(1)
-        .expect("first argument should be path to WAV file");
-    let audio_path = Path::new(&arg1);
-    if !audio_path.exists() {
-        panic!("audio file doesn't exist");
-    }
-    let arg2 = std::env::args()
-        .nth(2)
-        .expect("second argument should be path to Whisper model");
-    let whisper_path = Path::new(&arg2);
+    let whisper_path = PathBuf::from(
+        std::env::args()
+            .nth(1)
+            .expect("first argument should be path to audio file"),
+    );
     if !whisper_path.exists() {
         panic!("whisper file doesn't exist")
+    }
+    let audio_path = PathBuf::from(
+        std::env::args()
+            .nth(2)
+            .expect("second argument should be path to whisper model file"),
+    );
+    if !audio_path.exists() {
+        panic!("audio file doesn't exist");
     }
 
     let original_samples = parse_wav_file(audio_path);
@@ -53,7 +55,10 @@ fn main() {
     )
     .expect("failed to open model");
     let mut state = ctx.create_state().expect("failed to create key");
-    let mut params = FullParams::new(SamplingStrategy::default());
+    let mut params = FullParams::new(SamplingStrategy::BeamSearch {
+        beam_size: 5,
+        patience: -1.0,
+    });
     params.set_initial_prompt("experience");
     params.set_progress_callback_safe(|progress| println!("Progress callback: {}%", progress));
 
@@ -63,19 +68,9 @@ fn main() {
         .expect("failed to convert samples");
     let et = std::time::Instant::now();
 
-    let num_segments = state
-        .full_n_segments()
-        .expect("failed to get number of segments");
-    for i in 0..num_segments {
-        let segment = state
-            .full_get_segment_text(i)
-            .expect("failed to get segment");
-        let start_timestamp = state
-            .full_get_segment_t0(i)
-            .expect("failed to get start timestamp");
-        let end_timestamp = state
-            .full_get_segment_t1(i)
-            .expect("failed to get end timestamp");
+    for segment in state.as_iter() {
+        let start_timestamp = segment.start_timestamp();
+        let end_timestamp = segment.end_timestamp();
         println!("[{} - {}]: {}", start_timestamp, end_timestamp, segment);
     }
     println!("took {}ms", (et - st).as_millis());
